32e900d4f13f37cc2d22d88b359ae8bff3fc72db
Unknown <roger.levy@gmail.com>
2018-09-19 02:21:25 -0400

    and some help text

--- session.f
+++ session.f
@@ -6,4 +6,17 @@ true constant allegro-debug
     'source-id @ close-file drop
     s" test.f" file-exists [if]  ld test  [then]
     ide
+    cr .( Welcome to Ramen )
+    cr
+    cr .( To enter fullscreen hit ALT-ENTER. )
+    cr
+    cr .( You can `ld` any of the following: )
+    cr .( depth )
+    cr .( island )
+    cr .( rectland )
+    cr .( flies )
+    cr .( stickerknight )
+    cr .( kevin )
+    cr
+    cr .( You can clear this text with `wipe`. )
 [then]  
